c3bd50d8795545a17d86caeb680ca331c4ea4458
vlad <jan@jvaas.com>
2019-12-01 01:44:32 +0200

    improving DSL

--- src/main/kotlin/io/persephone/dsl/ScratchPad.kt
+++ src/main/kotlin/io/persephone/dsl/ScratchPad.kt
@@ -2,34 +2,40 @@ package io.persephone.helper
 
 import io.persephone.dsl.element.BODY
 import io.persephone.dsl.element.HEAD
+import io.persephone.dsl.element.HTML
 
 object ScratchPad {
 
 	@JvmStatic
 	fun main(args: Array<String>) {
 
+
+
 		val output1 =
-			HEAD().head {
-				title { +"this is the title" }
-			}
-
-		val output2 =
-			BODY().body {
-				div() {
-					ul {
-						li {
-							+"TEST1"
-						}
-						li {
-							+"TEST2"
-						}
-
-					}
+			HTML().head {
+				title {
+					+"this is the title"
 				}
+
 			}
+//
+//		val output2 =
+//			BODY().body {
+//				div() {
+//					ul {
+//						li {
+//							+"TEST1"
+//						}
+//						li {
+//							+"TEST2"
+//						}
+//
+//					}
+//				}
+//			}
 
 		println(output1)
-		println(output2)
+//		println(output2)
 
 	}
 
