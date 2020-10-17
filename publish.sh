047b2966c8dd21662e7183c30ea5ed9eadaf1a9b
vlad <jan@jvaas.com>
2020-10-17 16:28:32 +0200

    update licenses

---
+++ publish.sh
@@ -0,0 +1,12 @@
+
+rm -rf ~/.m2/repository/io/jvaas/jvaas-html
+rm -rf build
+
+gradle build 
+gradle publishToMavenLocal
+
+ls -lah ~/.m2/repository/io/jvaas/jvaas-html
+
+gsutil cp -R ~/.m2/repository/io/jvaas/jvaas-html gs://repo.jvaas.io/io/jvaas
+
+
