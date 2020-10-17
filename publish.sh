c591674739c91b9ac3b4d33cb95cefa5739fdf1f
vlad <jan@jvaas.com>
2020-10-17 18:22:37 +0200

    cleanup publish scripts.

--- publish.sh
+++ publish.sh
@@ -1,12 +1,13 @@
 
 rm -rf ~/.m2/repository/io/jvaas/jvaas-html
-rm -rf build
+#rm -rf build
 
-gradle build 
+gradle build
 gradle publishToMavenLocal
 
 ls -lah ~/.m2/repository/io/jvaas/jvaas-html
 
 gsutil cp -R ~/.m2/repository/io/jvaas/jvaas-html gs://repo.jvaas.io/io/jvaas
+
 
 
